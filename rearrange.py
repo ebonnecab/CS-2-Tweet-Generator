91ae59315be0738d890117206aa9f7f7981db581
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2019-01-24 18:23:49 -0800

    added rearrange module

--- rearrange.py
+++ rearrange.py
@@ -1,2 +1,9 @@
 #TO-DO: randomly rearrange a set of words; takes command-line arguments
 import random
+import sys
+
+def word_shuffle():
+    random.shuffle(sys.argv)
+    print(str(sys.argv))
+
+word_shuffle()
