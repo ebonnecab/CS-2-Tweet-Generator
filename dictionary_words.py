2882983d108e59d3a4ba538a392690c36c4fb920
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2019-01-29 21:48:53 -0800

    cleaned up white space and extra comments

--- dictionary_words.py
+++ dictionary_words.py
@@ -1,21 +1,18 @@
-#an algorithm that generates random sentences from words in a file.
+#an algorithm that generates random sentences from words in a file
 import random
+
 ''' This function accesses the word file, creates a list object, removes line breaks, and appends the words from the
-file to the list'''
+file to the list, takes five words from the list object, stores them in an array, and joins the array
+into a sentence before outputting it'''
 def random_sentence():
-#access and read the file
     with open('/usr/share/dict/words') as file:
-#created a list object, removed line breaks, and appended words from file
         words_list = []
         for words in file:
             words = words.rstrip("\n")
             words_list.append(words)
-#took four words from list object and stored them in an array
         word_array = random.choices(words_list, k=5)
-#joining words from array into a sentence
         sentence = ' '.join(word_array) + '.'
         sentence = sentence.capitalize()
-#output of sentence
         print(sentence)
 
 
