84535588866c06e9990b02574266110885f45bf3
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2019-01-29 21:46:53 -0800

    refactored random sentence function

--- dictionary_words.py
+++ dictionary_words.py
@@ -1,22 +1,23 @@
 #an algorithm that generates random sentences from words in a file.
 import random
-
+''' This function accesses the word file, creates a list object, removes line breaks, and appends the words from the
+file to the list'''
+def random_sentence():
 #access and read the file
-with open('/usr/share/dict/words') as file:
-
+    with open('/usr/share/dict/words') as file:
 #created a list object, removed line breaks, and appended words from file
-    words_list = []
-    for words in file:
-        words = words.rstrip("\n")
-        words_list.append(words)
-
+        words_list = []
+        for words in file:
+            words = words.rstrip("\n")
+            words_list.append(words)
 #took four words from list object and stored them in an array
-word_array = random.choices(words_list, k=4)
-
+        word_array = random.choices(words_list, k=5)
 #joining words from array into a sentence
-sentence = ' '.join(word_array) + '.'
-sentence = sentence.capitalize()
-
+        sentence = ' '.join(word_array) + '.'
+        sentence = sentence.capitalize()
 #output of sentence
-print(sentence)
+        print(sentence)
 
+
+if __name__ == '__main__':
+    random_sentence()
