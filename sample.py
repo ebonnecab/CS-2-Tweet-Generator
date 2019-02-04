33d33059f2457cdb9bab4fa6b4b86e308e6f47ad
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2019-02-04 00:47:25 -0800

    figuring out probability function

--- sample.py
+++ sample.py
@@ -13,14 +13,28 @@ accessing the histogram via the histogram function, iterating through the histog
 appending the words to the empty array, generating a random index, and returning that index
 '''
 def random_word(file):
-        words_list = []
-        histo = histogram.histogram(file)
-        for word in histo:
-            words_list.append(word)
-            rand_word = random.randint(1, len(histo)-1)
-        return words_list[rand_word]
+    words_list = []
+    histo = histogram.histogram(file)
+    for word in histo:
+        words_list.append(word)
+        rand_word = random.randint(1, len(histo)-1)
+    return words_list[rand_word]
 
+# from the word sample, determine the probability of the word randomly being chosen based upon its frequency
+def word_probability(file):
+    words_list = []
+    histo = histogram.histogram(file)
+    for word in histo:
+        words_list.append(word)
+    rand_word = words_list.random_word(file)
+    word_freq = int(histogram.frequency(rand_word, histo))
+    unique_words = int(histogram.unique_words(histo))
+    probability = float(word_freq/unique_words)
+    return probability
 
 if __name__ == '__main__':
-    random_word('siddhartha.txt')
+    test1 = random_word('siddhartha.txt')
+    test2 = word_probability('siddartha.txt')
+    print(test1)
+
     
